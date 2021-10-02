d0377f04ade2f7138c50eca8fadcb991010a896f
okusama27 <kaz@iron-hot.com>
2021-10-02 22:55:01 +0900

    Fixed code

--- sample_futures.py
+++ sample_futures.py
@@ -3,7 +3,7 @@ from sample_base import run_browser
 from concurrent import futures
 import time
 
-with futures.ThreadPoolExecutor() as executor:
-    for i in range(3):
+with futures.ThreadPoolExecutor(max_workers=2) as executor:
+    for i in range(5):
         future = executor.submit(run_browser, i)
         time.sleep(2)
--- sample_threading.py
+++ sample_threading.py
@@ -3,7 +3,11 @@ from sample_base import run_browser
 import time
 import threading
 
-for i in range(3):
+for i in range(5):
+    if threading.active_count() >= 2:
+        time.sleep(5)
+        continue
+
     thread = threading.Thread(target=run_browser, args=(i,))
     thread.start()
     time.sleep(2)
